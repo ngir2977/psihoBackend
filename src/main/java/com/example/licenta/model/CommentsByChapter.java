468cb47e0a6bbc93ad42bc346bb6f1bb99fde158
RobertMorati <robert.morati@she.net>
2023-05-11 16:29:59 +0300

    Edit add and get comments flow

--- src/main/java/com/example/licenta/model/CommentsByChapter.java
+++
@@ -1,9 +0,0 @@
-package com.example.licenta.model;
-
-public class CommentsByChapter {
-    public String chapter;
-
-    public String getChapter() {
-        return chapter;
-    }
-}
--- src/main/java/com/example/licenta/repository/CommentRepository.java
+++ src/main/java/com/example/licenta/repository/CommentRepository.java
@@ -1,13 +1,11 @@
 package com.example.licenta.repository;
 
 import com.example.licenta.model.Comment;
-import com.example.licenta.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public interface CommentRepository  extends JpaRepository<Comment,Integer> {
 
-    public ArrayList<Comment>  findAllByChapter (String chapter);
+    List<Comment>  findAllByChapterId(Integer chapterId);
 }
